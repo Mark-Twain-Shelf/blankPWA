54d28e650a9eb3ad3537131eded75654c92924eb
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-15 22:40:34 +0200

    path fix attend

--- scripts/sw.js
+++ scripts/sw.js
@@ -2,13 +2,13 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html", "manifest.json", "browserconfig.xml", "icons/pwa.svg"]);
+toolbox.precache(["../index.html", "../manifest.json", "../browserconfig.xml", "../icons/pwa.svg"]);
 
-toolbox.router.get('icons/*', toolbox.cacheFirst);
-toolbox.router.get('screenshots/*', toolbox.cacheFirst);
-toolbox.router.get('scripts/*', toolbox.cacheFirst);
-toolbox.router.get('css/*', toolbox.cacheFirst);
+toolbox.router.get('../icons/*', toolbox.cacheFirst);
+toolbox.router.get('../screenshots/*', toolbox.cacheFirst);
+toolbox.router.get('../scripts/*', toolbox.cacheFirst);
+toolbox.router.get('../css/*', toolbox.cacheFirst);
 
-toolbox.router.get('./*', toolbox.networkFirst, {
+toolbox.router.get('../*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
