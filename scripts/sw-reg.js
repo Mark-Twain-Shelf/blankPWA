d8947caea396612638176942af4a987b733decba
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-15 23:40:02 +0200

    simplify

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", (event) => {
     if ('serviceWorker' in navigator) {
-        const pathPrefix = "/bankPWA";
-        navigator.serviceWorker.register(`${pathPrefix}/scripts/sw.js`).then(function (registration) {
+        navigator.serviceWorker.register('scripts/sw.js').then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
             console.log('ServiceWorker registration failed: ', err);
