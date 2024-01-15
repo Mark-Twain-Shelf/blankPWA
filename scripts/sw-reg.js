0761f023caa8c54c93419188e21193e02d4d17c3
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-15 23:23:39 +0200

    path log

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", (event) => {
     if ('serviceWorker' in navigator) {
+        console.log('PATH: ', window.location.pathname);
         navigator.serviceWorker.register('scripts/sw.js').then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
--- scripts/sw.js
+++ scripts/sw.js
@@ -2,6 +2,8 @@
 
 importScripts('sw-toolbox.js');
 
+const path = window.location.pathname;
+console.log('SWPATH: ', path);
 toolbox.precache(["../index.html", "../manifest.json", "../browserconfig.xml", "../icons/pwa.svg"]);
 
 toolbox.router.get('../icons/*', toolbox.cacheFirst);
