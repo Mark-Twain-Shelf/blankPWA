195c8872dbf28c0593ea5dcab377014fc968d613
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-16 00:02:16 +0200

    fff

--- scripts/sw.js
+++ scripts/sw.js
@@ -2,13 +2,13 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["/bankPWA/index.html", "/bankPWA/manifest.json", "/bankPWA/browserconfig.xml", "/bankPWA/icons/pwa.svg"]);
+toolbox.precache(['/bankPWA/index.html', '/bankPWA/manifest.json', '/bankPWA/browserconfig.xml', '/bankPWA/icons/pwa.svg']);
 
-toolbox.router.get("/bankPWA/icons/*", toolbox.cacheFirst);
-toolbox.router.get("/bankPWA/screenshots/*", toolbox.cacheFirst);
-toolbox.router.get("/bankPWA/scripts/*", toolbox.cacheFirst);
-toolbox.router.get("/bankPWA/css/*", toolbox.cacheFirst);
+toolbox.router.get('/bankPWA/icons/*', toolbox.cacheFirst);
+toolbox.router.get('/bankPWA/screenshots/*', toolbox.cacheFirst);
+toolbox.router.get('/bankPWA/scripts/*', toolbox.cacheFirst);
+toolbox.router.get('/bankPWA/css/*', toolbox.cacheFirst);
 
-toolbox.router.get("/bankPWA/*", toolbox.networkFirst, {
+toolbox.router.get('/bankPWA/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
