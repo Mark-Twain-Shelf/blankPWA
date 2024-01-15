b9dfbe519e07289c3094b62c1849e37ea16ac97b
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-16 00:26:57 +0200

    Adapt to gh-pages (#1)
    changed pathes to relative

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,14 +1,14 @@
 'use strict';
 
-importScripts('/scripts/sw-toolbox.js');
+importScripts('sw-toolbox.js');
 
-toolbox.precache(["/index.html", "/manifest.json", "/browserconfig.xml", "/icons/pwa.svg"]);
+toolbox.precache(["../index.html", "../manifest.json", "../browserconfig.xml", "../icons/pwa.svg"]);
 
-toolbox.router.get('/icons/*', toolbox.cacheFirst);
-toolbox.router.get('/screenshots/*', toolbox.cacheFirst);
-toolbox.router.get('/scripts/*', toolbox.cacheFirst);
-toolbox.router.get('/css/*', toolbox.cacheFirst);
+toolbox.router.get("../icons/*", toolbox.cacheFirst);
+toolbox.router.get("../screenshots/*", toolbox.cacheFirst);
+toolbox.router.get("../scripts/*", toolbox.cacheFirst);
+toolbox.router.get("../css/*", toolbox.cacheFirst);
 
-toolbox.router.get('./*', toolbox.networkFirst, {
+toolbox.router.get("../*", toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
