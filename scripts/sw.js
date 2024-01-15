454bccd1445ad20477d90c2f0c856bb8fec90cb4
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-15 23:55:16 +0200

    explicit

--- scripts/sw.js
+++ scripts/sw.js
@@ -2,14 +2,13 @@
 
 importScripts('sw-toolbox.js');
 
-const pathPrefix = "/bankPWA";
-toolbox.precache([`${pathPrefix}/index.html`, `${pathPrefix}/manifest.json`, `${pathPrefix}/browserconfig.xml`, `${pathPrefix}/icons/pwa.svg`]);
+toolbox.precache(["/bankPWA/index.html", "/bankPWA/manifest.json", "/bankPWA/browserconfig.xml", "/bankPWA/icons/pwa.svg"]);
 
-toolbox.router.get(`${pathPrefix}/icons/*`, toolbox.cacheFirst);
-toolbox.router.get(`${pathPrefix}/screenshots/*`, toolbox.cacheFirst);
-toolbox.router.get(`${pathPrefix}/scripts/*`, toolbox.cacheFirst);
-toolbox.router.get(`${pathPrefix}/css/*`, toolbox.cacheFirst);
+toolbox.router.get("/bankPWA/icons/*", toolbox.cacheFirst);
+toolbox.router.get("/bankPWA/screenshots/*", toolbox.cacheFirst);
+toolbox.router.get("/bankPWA/scripts/*", toolbox.cacheFirst);
+toolbox.router.get("/bankPWA/css/*", toolbox.cacheFirst);
 
-toolbox.router.get(`${pathPrefix}/*`, toolbox.networkFirst, {
+toolbox.router.get("/bankPWA/*", toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
