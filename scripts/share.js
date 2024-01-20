ae71cfa94cce42de2f2dde8013c1f4c44f77ffd5
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-20 23:22:37 +0200

    reorg

--- scripts/share.js
+++ scripts/share.js
@@ -1,5 +1,24 @@
 import { debugMsg, logLevel } from './debug.js';
 
+function getInputFromUrl() {
+  const parsedUrl = new URL(window.location);
+  const title = parsedUrl.searchParams.get('title');
+  const text = parsedUrl.searchParams.get('text');
+  const url = parsedUrl.searchParams.get('url');
+  debugMsg(`Shared params: title=${title} text=${text} url=${url}`, logLevel.debug);
+  var str = "";
+  if (title) {
+    str = `<b>${title}</b><br/>`;
+  }
+  if (text) {
+    str += `${text}<br/>`;
+  }
+  if (url) {
+    str += `from: <a href="${url}">${url}</a><br/>`;
+  }
+  return str !== "" ? str : null;
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
   const contentBoxId = "mainEditor";
   const shareButtonId = "shareButton";
@@ -10,6 +29,10 @@ window.addEventListener("DOMContentLoaded", async () => {
   if (!contentBox) {
     debugMsg(`${contentBoxId} not found`, logLevel.error);
     return;
+  }
+  const str = getInputFromUrl();
+  if (str) {
+    contentBox.innerHTML = str;
   }
   const shareButton = document.getElementById(shareButtonId);
   if (!shareButton) {
@@ -36,31 +59,4 @@ window.addEventListener("DOMContentLoaded", async () => {
       debugMsg(`navigator.share failed with error: ${err}`, logLevel.error);
     });
   });
-});
-
-window.addEventListener("load", (event) => {
-  const contentBoxId = "mainEditor";
-  const contentBox = document.getElementById(contentBoxId);
-  if (!contentBox) {
-    debugMsg(`${contentBoxId} not found`, "error");
-    return;
-  }
-  var parsedUrl = new URL(window.location);
-  const title = parsedUrl.searchParams.get('title');
-  const text = parsedUrl.searchParams.get('text');
-  const url = parsedUrl.searchParams.get('url');
-  debugMsg(`Shared params: title=${title} text=${text} url=${url}`, logLevel.debug);
-  var str;
-  if (title) {
-    str = `<b>${title}</b><br/>`;
-  }
-  if (text) {
-    str += `${text}<br/>`;
-  }
-  if (url) {
-    str += `from: <a href="${url}">${url}</a><br/>`;
-  }
-  if (str) {
-    contentBox.innerHTML = str;
-  }
 });
