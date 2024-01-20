5f8801a063525e04c203746cc9c59df5a31963c7
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-20 19:16:17 +0200

    put sw.js in root

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -2,7 +2,7 @@ import { debugMsg, logLevel } from './debug.js';
 
 window.addEventListener("load", (event) => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('scripts/sw.js', {scope: '../'}).then(function (registration) {
+    navigator.serviceWorker.register('sw.js', {scope: location.origin}).then(function (registration) {
       debugMsg(`ServiceWorker registration successful with scope: ${registration.scope}`, logLevel.debug);
     }, function(err) {
       debugMsg(`ServiceWorker registration failed: ${err}`, logLevel.error);
--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 'use strict';
 
-importScripts('sw-toolbox.js');
+importScripts('scripts/sw-toolbox.js');
 
-toolbox.precache(["../index.html", "../manifest.json", "../browserconfig.xml", "../icons/pwa.svg"]);
+toolbox.precache(["index.html", "manifest.json", "browserconfig.xml", "icons/pwa.svg"]);
 
-toolbox.router.get("../icons/*", toolbox.cacheFirst);
-toolbox.router.get("../screenshots/*", toolbox.cacheFirst);
-toolbox.router.get("../scripts/*", toolbox.cacheFirst);
-toolbox.router.get("../css/*", toolbox.cacheFirst);
+toolbox.router.get("icons/*", toolbox.cacheFirst);
+toolbox.router.get("screenshots/*", toolbox.cacheFirst);
+toolbox.router.get("scripts/*", toolbox.cacheFirst);
+toolbox.router.get("css/*", toolbox.cacheFirst);
 
-toolbox.router.get("../*", toolbox.networkFirst, {
+toolbox.router.get("./*", toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
 
