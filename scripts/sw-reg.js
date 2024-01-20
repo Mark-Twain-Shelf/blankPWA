b673c1073dd18b2bd6c4b625508db8b4302425fa
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-20 20:40:05 +0200

    sw scope

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -1,8 +1,12 @@
+// ServicWorker registration scope
+// https://stackoverflow.com/questions/35358038/how-to-register-a-service-workers-scope-one-directory-above-where-the-service-w
+// https://stackoverflow.com/questions/49084718/how-exactly-add-service-worker-allowed-to-register-service-worker-scope-in-upp/49098917/
+
 import { debugMsg, logLevel } from './debug.js';
 
 window.addEventListener("load", (event) => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('scripts/sw.js').then(function (registration) {
+    navigator.serviceWorker.register('sw.js').then(function (registration) {
       debugMsg(`ServiceWorker registration successful with scope: ${registration.scope}`, logLevel.debug);
     }, function(err) {
       debugMsg(`ServiceWorker registration failed: ${err}`, logLevel.error);
---
+++ sw.js
@@ -0,0 +1,14 @@
+'use strict';
+
+importScripts('scripts/sw-toolbox.js');
+
+toolbox.precache(["index.html", "manifest.json", "browserconfig.xml", "icons/pwa.svg"]);
+
+toolbox.router.get("icons/*", toolbox.cacheFirst);
+toolbox.router.get("screenshots/*", toolbox.cacheFirst);
+toolbox.router.get("scripts/*", toolbox.cacheFirst);
+toolbox.router.get("css/*", toolbox.cacheFirst);
+
+toolbox.router.get("./*", toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
