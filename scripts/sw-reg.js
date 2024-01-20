600eed24d9c672cb61d4b6fb30657d25f8c15106
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-20 20:29:19 +0200

    refs

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -1,3 +1,7 @@
+// ServicWorker registration scope
+// https://stackoverflow.com/questions/35358038/how-to-register-a-service-workers-scope-one-directory-above-where-the-service-w
+// https://stackoverflow.com/questions/49084718/how-exactly-add-service-worker-allowed-to-register-service-worker-scope-in-upp/49098917/
+
 import { debugMsg, logLevel } from './debug.js';
 
 window.addEventListener("load", (event) => {
