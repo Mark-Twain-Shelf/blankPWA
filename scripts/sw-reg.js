3f5172716751f0295c31c040a8fe28c79589cd41
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-20 18:51:39 +0200

    sw scope

--- scripts/sw-reg.js
+++ scripts/sw-reg.js
@@ -2,7 +2,7 @@ import { debugMsg, logLevel } from './debug.js';
 
 window.addEventListener("load", (event) => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('scripts/sw.js').then(function (registration) {
+    navigator.serviceWorker.register('scripts/sw.js', {scope: '../'}).then(function (registration) {
       debugMsg(`ServiceWorker registration successful with scope: ${registration.scope}`, logLevel.debug);
     }, function(err) {
       debugMsg(`ServiceWorker registration failed: ${err}`, logLevel.error);
