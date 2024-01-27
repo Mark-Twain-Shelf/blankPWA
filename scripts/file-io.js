1361cbd112436dc1b9545e61f74c9fdf316675df
Oleg Fokin <oleg.fokin@gmail.com>
2024-01-27 21:54:10 +0200

    open file

--- scripts/file-io.js
+++ scripts/file-io.js
@@ -1,6 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
 // https://web.dev/articles/read-files
 // https://developer.chrome.com/docs/capabilities/web-apis/file-system-access
+// https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/offline#file-system-access
 // https://stackoverflow.com/a/45815534
 // https://stackoverflow.com/a/72012915
 
@@ -19,15 +20,11 @@ window.addEventListener("DOMContentLoaded", async () => {
     debugMsg(`${contentBoxId} not found`, logLevel.error);
     return;
   }
-  openButton.addEventListener('change', (e) => {
+  openButton.addEventListener('click', async () => {
     debugMsg(`File IO initiated`);
-    if (e.target.files && e.target.files[0]) {
-      var myFile = e.target.files[0];
-      var reader = new FileReader();
-      reader.addEventListener('load', function (e) {
-        contentBox.innerText = e.target.result;
-      });
-      reader.readAsBinaryString(myFile);
-    }
+    const [fileHandle] = await window.showOpenFilePicker();
+    const file = await fileHandle.getFile();
+    const content = await file.text();
+    contentBox.innerText = content;
   });
 });
